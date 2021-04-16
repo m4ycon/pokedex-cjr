655fc92db7a73a0cb3a230e73a62bf447f2d18f4
Maycon Fabio <m.vinnicyus@gmail.com>
2021-04-16 17:12:12 -0300

    feat: Loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,9 @@
+import { LoadingStyled } from "./styles"
+
+import pokeballImg from '../../assets/pokeball.png'
+
+const Loading = () => {
+  return <LoadingStyled src={pokeballImg} />
+}
+
+export default Loading
