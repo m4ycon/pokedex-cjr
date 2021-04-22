4e9a33fbbbaea2218a87ec394e6f37f9ba999058
Maycon Fabio <m.vinnicyus@gmail.com>
2021-04-21 23:21:06 -0300

    feat: componente Pokemon e listagem da home

---
+++ src/components/Pokemon/index.js
@@ -0,0 +1,76 @@
+import { ElementBall, FavoriteStar, PokemonContainer, PokemonName } from "./styles"
+
+import bugImg from '../../assets/elementos/bug.png'
+import darkImg from '../../assets/elementos/dark.png'
+import dragonImg from '../../assets/elementos/dragon.png'
+import eletricImg from '../../assets/elementos/eletric.png'
+import fairyImg from '../../assets/elementos/fairy.png'
+import fightingImg from '../../assets/elementos/fighting.png'
+import fireImg from '../../assets/elementos/fire.png'
+import flyingImg from '../../assets/elementos/flying.png'
+import ghostImg from '../../assets/elementos/ghost.png'
+import grassImg from '../../assets/elementos/grass.png'
+import groundImg from '../../assets/elementos/ground.png'
+import iceImg from '../../assets/elementos/ice.png'
+import normalImg from '../../assets/elementos/normal.png'
+import poisonImg from '../../assets/elementos/poison.png'
+import psychicImg from '../../assets/elementos/psychic.png'
+import rockImg from '../../assets/elementos/rock.png'
+import steelImg from '../../assets/elementos/steel.png'
+
+import emptyStarSvg from '../../assets/empty-star.svg'
+import starSvg from '../../assets/star.svg'
+
+
+export const Elements = ({ name }) => {
+
+  const elementsImg = {
+    bug: bugImg,
+    dark: darkImg,
+    dragon: dragonImg,
+    electric: eletricImg,
+    fairy: fairyImg,
+    fighting: fightingImg,
+    fire: fireImg,
+    flying: flyingImg,
+    ghost: ghostImg,
+    grass: grassImg,
+    ground: groundImg,
+    ice: iceImg,
+    normal: normalImg,
+    poison: poisonImg,
+    psychic: psychicImg,
+    rock: rockImg,
+    steel: steelImg,
+  }
+
+  return <ElementBall title={name}>
+    <img src={elementsImg[name]} alt={name} />
+  </ElementBall>
+}
+
+export const Favorite = ({ name, starred }) => {
+
+  return <FavoriteStar>
+    <img src={starred ? starSvg : emptyStarSvg} alt={name} />
+  </FavoriteStar>
+}
+
+export const HomePokemon = ({ pokemon, favorite }) => {
+  const elements = pokemon.kind.split(';')
+
+  return <PokemonContainer>
+    <img src={pokemon.image_url} alt={pokemon.name} />
+
+    {elements.map(element => <Elements key={element} name={element} />)}
+
+    <Favorite name={'star'} starred={favorite} />
+
+    <PokemonName>
+      <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+    </PokemonName>
+
+  </PokemonContainer>
+}
+
+export default HomePokemon
---
+++ src/components/Pokemon/styles.js
@@ -0,0 +1,121 @@
+import styled from 'styled-components'
+
+const containerSize = 150
+const ballImgSize = 40
+
+export const PokemonContainer = styled.div`
+  background-color: white;
+  width: ${containerSize}px;
+  height: ${containerSize}px;
+  margin: 20px;
+  padding: 5px;
+  position: relative;
+
+  border-radius: 50%;
+  border: 3px solid black;
+  border-top: 3px solid red;
+  border-right: 3px solid red;
+
+
+  :hover {
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    transition: transform ease-in-out 200ms;
+
+    :hover {
+    transform: scale(1.05);
+    }
+  }
+`
+
+
+const transformNBallPos = n => {
+  const oneDeg = Math.PI / 180
+  const circlePosDeg = (45 - (n-1) * 30) % 360
+
+  return `
+    &:nth-of-type(${n}) {
+      transform: translate(
+        calc(${Math.cos(oneDeg * circlePosDeg)} * ${containerSize / 2 + 5}px - 50%),
+        calc(${-Math.sin(oneDeg * circlePosDeg)} * ${containerSize / 2 + 5}px - 50%)
+      );
+    }
+  `
+}
+
+export const ElementBall = styled.div`
+  position: absolute;
+
+  top: 50%;
+  left: 50%;
+
+  width: ${ballImgSize}px;
+  height: ${ballImgSize}px;
+
+  img {
+    width: 100%;
+  }
+  
+  ${transformNBallPos(1)}
+  ${transformNBallPos(2)}
+  ${transformNBallPos(3)}
+  ${transformNBallPos(4)}
+
+`
+
+export const FavoriteStar = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  transform: translate(
+    calc(${Math.cos(Math.PI / 180 * 135)} * ${containerSize / 2 + 5}px - 50%),
+    calc(${-Math.sin(Math.PI / 180 * 135)} * ${containerSize / 2 + 5}px - 50%)
+  );
+
+  background-image: linear-gradient(90deg, whitesmoke 50%, lightgrey 50%);
+  width: 40px;
+  height: 40px;
+
+  border-radius: 50%;
+  border: 2px solid gold;
+  padding: 2px;
+  outline: none;
+  transition: filter ease-in-out 100ms;
+
+  :hover {
+    cursor: pointer;
+    filter: brightness(1.05);
+  }
+  :active {
+    filter: brightness(0.95);
+  }
+
+  img {
+    width: 30px;
+    height: 30px;
+
+    /* cor gold feito com filter pro svg */
+    filter: invert(71%) sepia(95%) saturate(545%) hue-rotate(358deg) brightness(102%) contrast(107%);
+  }
+
+`
+
+export const PokemonName = styled.div`
+  background-color: white;
+  padding: 5px 20px;
+  text-align: center;
+
+  position: absolute;
+  bottom: -5px;
+  left: 50%;
+  transform: translateX(-50%);
+
+  border-radius: 5px;
+  border: 2px solid black;
+`
+
