30b1b9e3d080afd7cea804af07a8cf9fba645ca7
Maycon Fabio <m.vinnicyus@gmail.com>
2021-04-22 14:22:59 -0300

    feat: página e rota 404

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
 
           <Route path="/perfil" component={Perfil} />
 
+          <Route component={NotFound} />
+
         </Switch>
       </AuthProvider>
     </BrowserRouter>
