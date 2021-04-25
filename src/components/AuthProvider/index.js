d05549b5a4cb597c6ca35ecc864ab033184350f5
Maycon Fabio <m.vinnicyus@gmail.com>
2021-04-24 22:47:36 -0300

    refactor: mudando lógica do login/out com contexto

--- src/components/AuthProvider/index.js
+++ src/components/AuthProvider/index.js
@@ -1,12 +1,29 @@
 import { createContext, useState } from 'react'
+import { apiUser } from '../../controllers/apiController'
 
-export const AuthContext = createContext([false, () => { }])
+export const AuthContext = createContext([false, () => { }, () => { }, () => { }])
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(false)
 
+  const login = async username => {
+
+    // tenta criar, se der erro é pq existe, dai procura o usuário
+    let newUser = false
+    try {
+      await apiUser.createUser(username)
+      newUser = await apiUser.searchUser(username)
+    } catch (error) {
+      newUser = await apiUser.searchUser(username)
+    } finally {
+      setUser(newUser)
+    }
+  }
+
+  const logout = () => setUser(false)
+
   return (
-    <AuthContext.Provider value={[user, setUser]}>
+    <AuthContext.Provider value={[user, setUser, login, logout]}>
       {children}
     </AuthContext.Provider>
   )
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,36 +1,14 @@
-import { useContext, useEffect } from "react"
+import { useContext, useState } from "react"
 import { Link } from "react-router-dom"
-import { apiUser } from "../../controllers/apiController"
 import { AuthContext } from "../AuthProvider"
+import Modal from "../Modal"
+import ModalLogin from "../ModalLogin"
 import { MenuStyled } from "./styles"
 
 export const Menu = () => {
+  const [showModal, setShowModal] = useState(false)
 
-  const [user, setUser] = useContext(AuthContext)
-
-  const login = async () => {
-    const username = 'ashh'
-
-    // tenta criar, se der erro é pq existe, dai procura o usuário
-    let newUser = false
-    try {
-      await apiUser.createUser(username)
-      newUser = await apiUser.searchUser(username)
-    } catch (error) {
-      newUser = await apiUser.searchUser(username)
-    } finally {
-      setUser(newUser)
-    }
-
-  }
-
-  const logout = async () => {
-    setUser(false)
-  }
-
-  useEffect(() => {
-    console.log(user)
-  }, [user])
+  const [user, , , logout] = useContext(AuthContext)
 
   return <MenuStyled>
     <Link to="/" className="home">Pokedex</Link>
@@ -38,13 +16,13 @@ export const Menu = () => {
     <div className="user">
       {user ?
         <>
-          <Link to="/perfil" className="perfil-btn">User</Link>
+          <Link to="/perfil" className="perfil-btn">Perfil {user.user.username}</Link>
           <button className="perfil-btn" onClick={logout}>Logout</button>
         </> :
-        <button className="perfil-btn" onClick={login}>Login</button>}
-
-
+        <button className="perfil-btn" onClick={() => setShowModal(true)}>Login</button>}
     </div>
+
+    {(showModal && !user) && <Modal setIsVisible={setShowModal} ><ModalLogin /></Modal>}
   </MenuStyled>
 }
 
